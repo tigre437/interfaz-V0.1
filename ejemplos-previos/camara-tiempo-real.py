9a5e910bdc37eacc5b73a723f45ed5d06bf41c80
David Serrano Henares <davidserranojuegos@gmail.com>
2024-03-19 13:40:41 +0100

    añado gitignore y set-up

--- ejemplos-previos/camara-tiempo-real.py
+++ ejemplos-previos/camara-tiempo-real.py
@@ -6,15 +6,7 @@ import cv2
 from PyQt6.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
 import numpy as np
 
-# requires python >= 3.9
-# from pygrabber.dshow_graph import FilterGraph
-# def get_available_cameras():
-#     devices = FilterGraph().get_input_devices()
-#     available_cameras = {}
 
-#     for device_index, device_name in enumerate(devices):
-#         available_cameras[device_index] = device_name
-#     return available_cameras
 
 
 class VideoThread(QThread):
